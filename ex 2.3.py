89f1fbefbae945724f47ecdd7cea3917ed5d12c9
Gelinator <gelinas.dave@gmail.com>
2015-03-30 14:26:11 -0400

    reorganize folders

--- ex 2.3.py
+++
@@ -1,6 +0,0 @@
-hours = float(raw_input("Enter hours:\n"))
-rate = float(raw_input("Enter rate:\n"))
-
-Pay = hours * rate
-
-print Pay
--- ex 2.5.py
+++
@@ -1,3 +0,0 @@
-temp = raw_input("What is the temperature (Celsius): \n")
-
-print float(temp) * 1.8 + 32
